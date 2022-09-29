1d47c8e3b4b7530d658add643ba0a39a8cf442d5
qiaoxin <1335994390@qq.com>
2022-09-29 10:15:00 +0800

    引用mock,编写模拟接口

---
+++ src/mock/login.ts
@@ -0,0 +1,15 @@
+import Mock from "mockjs";
+const Random = Mock.Random;
+
+Mock.setup({
+  //设置请求延时时间
+  timeout: 1000,
+});
+
+let datalist = {
+  code: 200,
+  msg: "获取成功!",
+  data: [] as Array<any>,
+};
+
+Mock.mock("http://127.0.0.1:5173/api/login", "POST", datalist);
